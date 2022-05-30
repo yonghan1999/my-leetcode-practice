e24e98458eab54999429d3e460459cdaf7a8a165
yuuki <yong.han.life@outlook.com>
2022-05-30 15:14:57 +0800

    move zeroes 双指针记录

--- move-zeroes/src/lib.rs
+++ move-zeroes/src/lib.rs
@@ -2,17 +2,17 @@
 struct Solution {}
 
 
-// 魔改冒泡排序
+// 双指针
 impl Solution {
     pub fn move_zeroes(nums: &mut Vec<i32>) {
+        let mut j = 0;
         for i in 0..nums.len() {
-            for j in i + 1..nums.len() {
-                if nums[i] == 0 {
-                    let t = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = t;
+            if nums[i] != 0 {
+                nums[j] = nums[i];
+                if i != j {
+                    nums[i] = 0;
                 }
-                else { break; }
+                j+=1;
             }
         }
     }
