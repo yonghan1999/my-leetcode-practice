45abf6c3fcd8e3c077ba460e2538f82753243dce
Yuuki <yong.han.life@outlook.com>
2022-07-11 16:39:15 +0800

    最接近的二叉搜索树值

--- closest-binary-search-tree-value/src/lib.rs
+++ closest-binary-search-tree-value/src/lib.rs
@@ -20,7 +20,21 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn closest_value(root: Option<Rc<RefCell<TreeNode>>>, target: f64) -> i32 {
+        if let Some(ref rf) = root {
+            let mut val = rf.borrow().val;
+            let mut closet = val;
+            let mut r = root;
 
+            while let Some(ref node) = r {
+                val = node.borrow().val;
+                closet = if (val as f64 - target).abs() < (closet as f64 - target).abs() { val } else { closet };
+                r = if target < val as f64 { node.borrow_mut().left.take() } else { node.borrow_mut().right.take() };
+            }
+
+            closet
+        } else {
+            -1
+        }
     }
 }
 
